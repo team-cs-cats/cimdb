4e646de8e65c6bf1042b1899526b2e0eaaea5586
Asa Holland <hollandasa@gmail.com>
2021-01-28 18:34:10 -0800

    added some basic html pages

--- cim/webapp.py
+++ cim/webapp.py
@@ -13,3 +13,25 @@ webapp = Flask(__name__)
 def index():
     """The webapp's landing page."""
     return render_template("index.html")
+
+@webapp.route('/workorders')
+def workorders():
+    """The webapp's page for work orders, which allows reviewing and adding work orders."""
+    return render_template("workorders.html")
+
+@webapp.route('/products')
+def products():
+    """The webapp's page for viewing an employee's currently assigned products to assemble and QC."""
+    return render_template("products.html")
+
+@webapp.route('/inventory')
+def inventory():
+    """The webapp's page for viewing the inventory.
+    This allows the employee to review existing stock and order new stock of standard and special components."""
+    return render_template("inventory.html")
+
+@webapp.route('/user_management')
+def user_management():
+    """The webapp's page for managing current users.
+    This allows a manager to update information about current users."""
+    return render_template("user_management.html")
