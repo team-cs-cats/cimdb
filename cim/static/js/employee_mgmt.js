fb964b0fb74c75a7d85e7199ceb40b01f796935e
Asa Holland <hollandasa@gmail.com>
2021-02-23 23:53:21 -0800

    moved javascript out of employee & regular comp HTML pages

---
+++ cim/static/js/employee_mgmt.js
@@ -0,0 +1,30 @@
+// employee_mgmt.js
+
+// Loads the Edit Modal popup for the employee edit function
+$(document).ready(function(){
+		$("#editEmployee").on("show.bs.modal", function(event){
+	// Get the button that triggered the modal
+	var button = $(event.relatedTarget);
+
+	// Extract value from the custom data-* attribute
+	var titleData = button.data("title");
+	$(this).find(".modal-title").text(titleData);
+
+	var employeeFirstNameData = button.data("employee-first-name");
+	$(this).find("#edit-employee-first-name").val(employeeFirstNameData);
+
+	var employeeLastNameData = button.data("employee-last-name");        
+	$(this).find("#edit-employee-last-name").val(employeeLastNameData);
+
+	var employeeGroupData = button.data("employee-group");
+	$(this).find("#edit-employee-group").val(employeeGroupData);
+
+	var employeeSiteData = button.data("employee-site");
+	$(this).find("#edit-employee-site").val(employeeSiteData);
+
+	var employeeEmailData = button.data("employee-email");
+	$(this).find("#edit-employee-email").val(employeeEmailData);
+
+	
+});
+	});
