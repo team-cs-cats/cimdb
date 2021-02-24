2de5d9393b53ef9b49c1c934788fa2038662ec61
Asa Holland <hollandasa@gmail.com>
2021-02-23 23:58:24 -0800

    moved locations JS out of HTML

--- cim/static/js/locations.js
+++ cim/static/js/locations.js
@@ -1,4 +1,21 @@
-console.log('Hello from locations.js!');
 
+// open locations edit modal popup
+$(document).ready(function(){
+    $("#editLocation").on("show.bs.modal", function(event){
+        // Get the button that triggered the modal
+        var button = $(event.relatedTarget);
 
+        // Extract value from the custom data-* attribute
+        var titleData = button.data("title");
+        $(this).find(".modal-title").text(titleData);
 
+        var roomNumberData = parseInt(button.data("location-room-number"));
+        $(this).find("#room-number").val(roomNumberData);
+
+        var shelfNumberData = parseInt(button.data("location-shelf-number"));
+        $(this).find("#shelf-number").val(shelfNumberData);
+
+        var siteNameData = button.data("site-name");        
+        $(this).find("#location-site").val(siteNameData);
+    });
+});
