62ad7e8d02adc257a6b4a9314987204fa1c25dbb
Asa Holland <hollandasa@gmail.com>
2021-02-24 22:17:42 -0800

    refactoring locations insert with JS

--- cim/static/js/locations.js
+++ cim/static/js/locations.js
@@ -19,3 +19,36 @@ $(document).ready(function(){
         $(this).find("#location-site").val(siteNameData);
     });
 });
+
+
+
+//add a new location
+function add_new_location(){
+
+    // create a new data constant that packages this collected information
+    const data = {
+
+        // Pull the selected site id, new room number and new shelf number from the add new location form 
+        add_new_location_site : this.add_new_location_site.value, 
+        add_location_room_number : this.add_location_room_number.value, 
+        add_location_shelf_number : this.add_location_shelf_number.value 
+    };
+
+
+  fetch('/locations', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Success:', data);
+    location.reload()
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+}
+
