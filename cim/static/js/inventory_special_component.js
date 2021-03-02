c04521e071c52f8eae2f41c260abba9f864b11f4
Asa Holland <hollandasa@gmail.com>
2021-03-01 23:24:34 -0800

    attempting to fix the UPDATE for special components

--- cim/static/js/inventory_special_component.js
+++ cim/static/js/inventory_special_component.js
@@ -14,16 +14,7 @@ $(document).ready(function(){
 
 	var specCompLocationData = button.data("spec-comp-edit-location");        
 	$(this).find("#spec-comp-edit-location").val(specCompLocationData);
-
-	// var siteNameData = button.data("spec-comp-site-name");        
-	// $(this).find("#spec-comp-edit-site").val(siteNameData);
-
-	// var roomNumberData = parseInt(button.data("spec-comp-room-number"));
-	// $(this).find("#spec-comp-edit-room-number").val(roomNumberData);
-
-	// var shelfNumberData = parseInt(button.data("spec-comp-shelf-number"));
-	// $(this).find("#spec-comp-edit-shelf-number").val(shelfNumberData);
-
+	
 	// Referenced this StackOverflow discussion to determine how to adject the checked property using jquery
 	// https://stackoverflow.com/questions/426258/setting-checked-for-a-checkbox-with-jquery/426276#426276 
 	var specialComponentIsFreeData = button.data("spec-comp-is-free");        
