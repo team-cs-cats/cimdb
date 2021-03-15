866d9e73c10204969bee618435ac4ef0807d3cfb
Asa Holland <hollandasa@gmail.com>
2021-03-14 21:07:23 -0700

    update ship modal

--- cim/static/js/shipping.js
+++ cim/static/js/shipping.js
@@ -2,7 +2,7 @@
 
 // shipping modal (View Work Order)
 $(document).ready(function(){
-    $("#viewWorkOrder").on("show.bs.modal", function(event){
+    $("#shipWorkOrder").on("show.bs.modal", function(event){
         // Get the button that triggered the modal
         var button = $(event.relatedTarget);
 
@@ -12,9 +12,6 @@ $(document).ready(function(){
 
         var woIDdata = parseInt(button.data("work_order_id"));
         $(this).find("#view-work-order-id").val(woIDdata);
-
-        var woOpenDate = button.data("work-order-open-date");        
-        $(this).find("#view-work-order-open-date").val(woOpenDate);
 
         var woRefNumData = button.data("work-order-reference-number");
         $(this).find("#view-work-order-reference-number").val(woRefNumData);
